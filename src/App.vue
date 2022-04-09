eb828d0866ed5e431d026776a440f6fc5243e9b5
Jay Arredondo <jearredondo13@gmail.com>
2022-04-09 17:11:34 -0500

    finished up delete and toggle reminder functionality

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,61 @@
 <template>
 <div class="container">
   <Header title="Task Manager"/>
+  <Tasks :tasks="tasks" @delete-task="deleteTask" @toggle-reminder="toggleReminder"/>
 </div>
 </template>
 
 <script>
 import Header from './components/Header.vue'
-
+import Tasks from './components/Tasks.vue'
 export default {
   name: 'App',
   components: {
-    Header,
+    Header, 
+    Tasks
+  },
+  data () {
+    return {
+      tasks: []
+    }
+  },
+  methods: {
+    deleteTask(id) {
+      if(confirm("Are you sure you want to delete?")){
+      this.tasks = this.tasks.filter((task) => task.id !== id)
+      }
+    },
+    toggleReminder(id) {
+      this.tasks = this.tasks.map((task) => 
+        task.id === id ? {...task, reminder : !task.reminder} : task
+        
+      )
+    }
+  },
+  created () {
+    this.tasks = [
+      {
+        id: 1, 
+        text: "Doctors Appointment",
+        day: "March 1st at 2:30pm", 
+        reminder :true
+      },
+      {
+        id: 2, 
+        text: "Meeting at School", 
+        day: "March 3rd at 1:30pm", 
+        reminder: true
+      }, 
+      {
+        id: 3, 
+        text: "Food Shopping", 
+        day: "March 3rd at 11:00am", 
+        reminder: false
+      }
+    ]
   }
 }
+
 </script>
 
 <style>
---
+++ src/components/Button.vue
@@ -0,0 +1,18 @@
+<template>
+    <button @click="onClick()" :style="{background: color}"  class="btn">{{text}}</button>
+</template>
+
+<script>
+    export default {
+        name: 'Button',
+        props: {
+            color: String,
+            text: String,
+        },
+        methods: {
+            onClick() {
+                console.log('click');
+            }
+        }
+    }
+</script>
